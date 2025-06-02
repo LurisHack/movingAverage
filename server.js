9fc1b925a4d1b1d573ca2141f7287ce08b73eab3
luris <luris5899@gmail.com>
2025-06-02 15:45:35 +0630

    server.js next 2

---
+++ server.js
@@ -0,0 +1,21 @@
+import express  from 'express';
+import path from 'path';
+import {init} from "./src/init.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// // Serve static files from the "public" directory
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// // Default route to serve index.html
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
+
+
+await init();
