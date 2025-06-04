1c292fb0164cc06537636d58cca75eec6beb1b9a
luris <luris5899@gmail.com>
2025-06-04 10:31:09 +0630

    Trend only short interval 15 minutes next 1

--- src/utility/trendDetector.js
+++ src/utility/trendDetector.js
@@ -242,10 +242,10 @@ export async function scanMarkets(limit, interval = '15m') {
 
 
 
-const result = await scanMarkets(100);
+// const result = await scanMarkets(100);
 // console.log("📉 Downtrend Coins (sorted by % drop):");
 // result.downtrends.forEach(d => {
 //     console.log(`${d.symbol} @ ${d.price.toFixed(4)} 🔻 ${d.percentDrop.toFixed(2)}%`);
 // });
 
-console.log(result);
+// console.log(result);
